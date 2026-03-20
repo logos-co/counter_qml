1b97442eccf8508882154d6d59e1b302c4e60880
Iuri Matias <iuri.matias@gmail.com>
2026-03-20 06:40:54 -0400

    follow logos-nix for nix packages

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Counter QML Plugin for Logos - A simple counter implemented entirely in QML";
 
   inputs = {
-    logos-cpp-sdk.url = "github:logos-co/logos-cpp-sdk";
-    nixpkgs.follows = "logos-cpp-sdk/nixpkgs";
+    logos-nix.url = "github:logos-co/logos-nix";
+    nixpkgs.follows = "logos-nix/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, logos-cpp-sdk }:
+  outputs = { self, logos-nix, nixpkgs }:
     let
       systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
